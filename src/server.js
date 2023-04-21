730d6292d13e3ec5e25850a4d4337292a6ab3e78
Jose Martinez <juniorjose1925@gmail.com>
2023-04-21 08:36:39 -0300

    add ecosistems and .env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 require("express-async-errors");
+require("dotenv/config");
+
+
+const uploadConfig = require("./configs/upload");
+
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require ("./utils/AppError");
-const uploadConfig = require("./configs/upload");
 
 
 const cors = require("cors");
@@ -40,7 +44,7 @@ app.use(( error, request, response, next) =>{
 } );
 
 
-const PORT = 3333;
+const PORT = process.env.PORT  || 3333;
 /*criamos o endereço ou seja o numero da porta */
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
